016fc2b8d9dba70a01bb18a40411c1a8a00cf871
Mistercleng <mrcleng@gmail.com>
2020-11-07 20:17:10 -0400

    V2 correções de testes

--- src/test/java/school/cesar/eta/unit/PersonTest.java
+++ src/test/java/school/cesar/eta/unit/PersonTest.java
@@ -3,104 +3,120 @@ package school.cesar.eta.unit;
 import org.easymock.IMockBuilder;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import sun.jvm.hotspot.utilities.Assert;
 
 import static org.mockito.Mockito.*;
 import java.time.LocalDate;
 import java.time.Year;
+import java.util.List;
 
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 public class PersonTest {
+    @InjectMocks
+    Person mockperson;
+    @Spy
+    List<Person> family;
 
     @Test
     public void getName_firstNameJonLastNameSnow_jonSnow() {
-        Person mockperson = mock(Person.class);
-        when(mockperson.getFirstName()).thenReturn("Jon");
-        when(mockperson.getLastName()).thenReturn("Snow");
-        assertEquals("Jon",mockperson.getFirstName());
-        assertEquals("Snow",mockperson.getLastName());
+        Person person = new Person();
+        person.setName("Jon");
+        person.setLastName("Snow");
+        String name = person.getName();
+        assertEquals("JonSnow",name);
     }
 
     @Test
     public void getName_firstNameJonNoLastName_jon() {
-        Person mockperson = mock(Person.class);
-        when(mockperson.getFirstName()).thenReturn("Jon");
-        when(mockperson.getLastName()).thenReturn("");
-        assertEquals("Jon",mockperson.getFirstName());
-        assertEquals("",mockperson.getLastName());
+        Person person = new Person();
+        person.setName("Jon");
+        String name = person.getName();
+        assertEquals("Jon",name);
     }
 
     @Test
     public void getName_noFirstNameLastNameSnow_snow() {
-        Person mockperson = mock(Person.class);
-        when(mockperson.getFirstName()).thenReturn("");
-        when(mockperson.getLastName()).thenReturn("Snow");
-        assertEquals("",mockperson.getFirstName());
-        assertEquals("Snow",mockperson.getLastName());
-
+        Person person = new Person();
+        person.setLastName("Snow");
+        String name = person.getName();
+        assertEquals("Snow",name);
     }
 
     @Test
     public void getName_noFirstNameNorLastName_throwsException() {
-        Person mockperson = mock(Person.class);
-        when(mockperson.getName()).thenThrow(new RuntimeException());
-
-        Exception exception = assertThrows(RuntimeException.class, () -> {
-            Integer.parseInt("Name must be filled");
-        });
-
-        String expectedMessage = "Name must be filled";
-        String actualMessage = exception.getMessage();
-
-        assertTrue(actualMessage.contains(expectedMessage));
-
+        Person person = new Person();
+        Exception thrown = assertThrows(Exception.class, () -> person.getName(),
+        "Name must be filled");
+        assertTrue(thrown.getMessage().contains("Name must be filled"));
     }
 
     @Test
     public void isBirthdayToday_differentMonthAndDay_false() {
-        Person mockperson = mock(Person.class);
-        when(mockperson.getNow()).thenReturn(LocalDate.of(1989,02,21));
-        when(mockperson.isBirthdayToday()).thenReturn(false);
-        assertFalse(mockperson.isBirthdayToday());
+        Person person = new Person(){
+            @Override
+            public LocalDate getNow() {
+              return LocalDate.of(1989,03,23);
+            }
+        };
+
+        person.setBirthday(LocalDate.of(1989,04,22));
+
+        assertFalse(person.isBirthdayToday());
 
     }
 
     @Test
     public void isBirthdayToday_sameMonthDifferentDay_false() {
-        Person mockperson = mock(Person.class);
-        when(mockperson.getNow()).thenReturn(LocalDate.of(1989,02,21));
-        when(mockperson.isBirthdayToday()).thenReturn(false);
-        assertFalse(mockperson.isBirthdayToday());
+        Person person = new Person(){
+            @Override
+            public LocalDate getNow() {
+                return LocalDate.of(1989,03,23);
+            }
+        };
 
-//        fail();
+        person.setBirthday(LocalDate.of(1989,03,22));
+
+        assertFalse(person.isBirthdayToday());
+
     }
 
     @Test
     public void isBirthdayToday_sameMonthAndDay_true() {
+        Person person = new Person(){
+            @Override
+            public LocalDate getNow() {
+                return LocalDate.of(1989,03,23);
+            }
+        };
 
-        Person mockperson = mock(Person.class);
-        when(mockperson.getNow()).thenReturn(LocalDate.of(1989,02,21));
-        when(mockperson.isBirthdayToday()).thenReturn(true);
-        assertTrue(mockperson.isBirthdayToday());
+        person.setBirthday(LocalDate.of(1989,03,23));
+
+        assertTrue(person.isBirthdayToday());
 
 
     }
 
+
     @Test
     public void addToFamily_somePerson_familyHasNewMember() {
         Person person = new Person();
-        person.addToFamily(person);
-        assertTrue(person.isFamily(person));
+        mockperson.addToFamily(person);
+        verify(family, times(1)).add(person);
     }
 
     @Test
     public void addToFamily_somePerson_personAddedAlsoHasItsFamilyUpdated() {
         Person person = new Person();
-        person.addToFamily(person);
-        assertTrue(person.isFamily(person));
-
+        person.addToFamily(mockperson);
+        verify(family, times(1)).add(person);
     }
 
     @Test
@@ -112,29 +128,10 @@ public class PersonTest {
 
     @Test
     public void isFamily_relativePerson_true() {
-        Person person = new Person();
         Person relative = new Person();
-        person.addToFamily(relative);
-        assertTrue(person.isFamily(relative));
+        when(family.contains(relative)).thenReturn(true);
+        assertTrue(mockperson.isFamily(relative));
     }
-
-
-//    public int diaAlterado(int dia){
-//        if(dia >= 29) {
-//            return dia -= 1;
-//        }else{
-//            return dia +=1;
-//        }
-//    }
-//
-//    public int mesAlterado(int mes){
-//        if(mes >= 29) {
-//            return mes -= 1;
-//        }else{
-//            return mes +=1;
-//        }
-//    }
-//
 
 
 }
